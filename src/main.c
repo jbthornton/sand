f1f8d84351e949d6f01013fb84bcdf6614f2d1e4
leaf <thorntonjamesb@gmail.com>
2025-11-15 23:00:46 -0700

    purple window, can be closed, also closes when esc pressed

--- src/main.c
+++ src/main.c
@@ -7,11 +7,15 @@
 struct Game{
 	SDL_Window* window;
 	SDL_Renderer* renderer;
+	SDL_Event event;
+	bool is_running;
 };
 
 bool game_init_sdl(struct Game *g);
-void game_free(struct Game *g);
+bool game_init(struct Game **game);
+void game_free(struct Game **game);
 void game_run(struct Game *g);
+void game_draw(struct Game *g);
 
 bool game_init_sdl(struct Game *g){
 	if(!SDL_Init(SDL_INIT_VIDEO)){
@@ -34,33 +38,86 @@ bool game_init_sdl(struct Game *g){
 	return true;
 }
 
-void game_free(struct Game *g){
-	if(g->window){
-		SDL_DestroyWindow(g->window);
-		g->window = NULL;
+bool game_init(struct Game **game){
+	*game = calloc(1,sizeof(struct Game));
+	if(*game == NULL){
+		fprintf(stderr, "Could not calloc game\n");
+		return false;
+	}
+	struct Game *g = *game;
+
+	if(!game_init_sdl(g)){
+		return false;
 	}
 
-	if(g->renderer){
-		SDL_DestroyRenderer(g->renderer);
-		g->renderer = NULL;
-	}
+	g->is_running = true;
+	return true;
+}
 
-	SDL_Quit();
+void game_free(struct Game **game){
+	if(*game){
+		struct Game *g = *game;
+		if(g->window){
+			SDL_DestroyWindow(g->window);
+			g->window = NULL;
+		}
+
+		if(g->renderer){
+			SDL_DestroyRenderer(g->renderer);
+			g->renderer = NULL;
+		}
+
+		SDL_Quit();
+	
+		free(g);
+		g = NULL;
+		*game = NULL;
+	}
+}
+
+void game_events(struct Game *g){
+	while(SDL_PollEvent(&g->event)){
+		switch(g->event.type){
+		case SDL_EVENT_QUIT:
+			g->is_running = false;
+			break;
+		case SDL_EVENT_KEY_DOWN:
+			switch(g->event.key.scancode){
+			case SDL_SCANCODE_ESCAPE:
+				g->is_running = false;
+				break;
+			default:
+				break;
+			}
+			break;
+		default:
+			break;
+		}
+
+	}
+}
+
+void game_draw(struct Game *g){
+	SDL_SetRenderDrawColor(g->renderer, 128, 0, 255, 255);
+	SDL_RenderClear(g->renderer);
+	SDL_RenderPresent(g->renderer);
 }
 
 void game_run(struct Game *g){
-	SDL_RenderClear(g->renderer);
-	SDL_RenderPresent(g->renderer);
-	SDL_Delay(5000);
+	while(g->is_running){
+		game_events(g);
+		game_draw(g);
+		SDL_Delay(16);//run ~60 times per second
+	}
 }
 
 int main(){
 	int exit_status = EXIT_FAILURE;
-	struct Game game = {0};
-	if(game_init_sdl(&game)){
+	struct Game *game = NULL;
+	if(game_init(&game)){
 		exit_status = EXIT_SUCCESS;
 	}
-	game_run(&game);
+	game_run(game);
 
 	game_free(&game);
 	return exit_status;
